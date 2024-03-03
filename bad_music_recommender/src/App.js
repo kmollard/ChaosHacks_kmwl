32eb3c6dd47df89fdf3097f8f050ed49826d9b6a
Syfirry <syfirry@gmail.com>
2024-03-02 16:31:20 -0800

    Added form submission animation

--- bad_music_recommender/src/App.js
+++ bad_music_recommender/src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [submitted, setSubmitted] = useState(false);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSubmitted(true);
+    console.log("Runs");
+  }
+
+
   return (
     <div className="App">
-      <div className="landingPage">
-        <p id="introText">
-          Chaotify
-        </p>
-        <input type="text" id="genreInput" autocomplete="off" placeholder=" Who's your favourite artist?"></input>
+      <div className={`landingPage ${submitted ? 'slideOut' : ''}`}>
+        <div className="title">
+          <img id="chaotifyLogo" src="ChaotifyLogo.png" alt="Chaotify logo"></img>
+          <p id="introText">
+            Chaotify
+          </p>
+        </div>
+        <form name="search" method="post" onSubmit={handleSubmit}>
+          <input type="text" id="genreInput" autocomplete="off" placeholder="What genre do you want to listen to?" />
+          <input type="submit" hidden />
+        </form>
+      </div>
+      <div className="musicPlayer">
+        
       </div>
     </div>
   );
