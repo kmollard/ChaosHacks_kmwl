00df3ee803065a5be4a3fb1dc3135d34dbe918bf
Syfirry <syfirry@gmail.com>
2024-03-02 17:50:42 -0800

    added player mockup

--- bad_music_recommender/src/App.js
+++ bad_music_recommender/src/App.js
@@ -3,16 +3,40 @@ import React, { useState } from 'react';
 
 function App() {
   const [submitted, setSubmitted] = useState(false);
+  const [toggled, setToggled] = useState(false);
   function handleSubmit(e) {
     e.preventDefault();
     setSubmitted(true);
-    console.log("Runs");
+    console.log(e.target.artist.value);
+  }
+
+  function changeButton(e) {
+    e.preventDefault();
+    setToggled(!toggled);
   }
 
 
   return (
-    <div className="App">
-      <div className={`landingPage ${submitted ? 'slideOut' : ''}`}>
+    <div className="App parent">
+      <div className={`audioPlayer child ${submitted ? 'slideIn' : ''}`}>
+        <div className="songCover">
+          <img id="songCover" src="justinbieberbaby.png" alt="Song cover"></img>
+        </div>
+        <div className="songInfo">
+          <p id="songName">
+            Baby
+          </p>
+          <p id="songArtist">
+            Justin Bieber
+          </p>
+        </div>
+        <div className="playBar">
+          <input type="button" name="play" id="playButton" value="⏮" />
+          <input type="button" name="play" id="playButton" value={`${toggled ? '⏸' : '⏵'}`} onClick={changeButton} />
+          <input type="button" name="play" id="playButton" value="⏭" />
+        </div>
+      </div>
+      <div className={`landingPage child ${submitted ? 'slideOut' : ''}`}>
         <div className="title">
           <img id="chaotifyLogo" src="ChaotifyLogo.png" alt="Chaotify logo"></img>
           <p id="introText">
@@ -20,12 +44,9 @@ function App() {
           </p>
         </div>
         <form name="search" onSubmit={handleSubmit}>
-          <input type="text" id="genreInput" autocomplete="off" placeholder="What artist do you want to listen to?" />
+          <input type="text" name="artist" id="genreInput" autocomplete="off" placeholder="What artist do you want to listen to?" />
           <input type="submit" hidden />
         </form>
-      </div>
-      <div className="musicPlayer">
-
       </div>
     </div>
   );
